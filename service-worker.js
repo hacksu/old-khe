5ace752ad496bc4e923d4944ba68c7625670c5b3
Paul Dilyard <paul.dilyard@gmail.com>
2015-09-20 01:19:30 -0400

    Make service-worker pull most recent message from API

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,41 @@
+var api = 'https://api.khe.io/v1.0';
+var img = 'https://khe.io/img/logo-short-square-black.png';
+var tag = 'simple-push-demo-notification-tag';
+
 self.addEventListener('push', function (event) {
 
   event.waitUntil(
-    fetch(config.api + '/messages').then(function (response) {
+    fetch(api + '/messages').then(function (response) {
       return response.json().then(function (data) {
-        console.log(data);
-        var title = data.notification.title;
-        var message = data.notification.message;
-        var icon = '/img/logo-short-square-black.png';
-        var tag = 'simple-push-demo-notification-tag';
 
-        return self.registration.showNotification(title, {
-          body: message,
-          icon: icon,
-          tag: tag
-        });
+        if (data.messages.length > 0) {
+
+          var title = 'KHE Update';
+          var message = data.messages[0].text;
+
+          return self.registration.showNotification(title, {
+            body: message,
+            icon: img,
+            tag: tag
+          });
+
+        } else {
+
+          return self.registration.showNotification('KHE Update', {
+            body: 'Check out khe.io/live for more info!',
+            icon: img,
+            tag: tag
+          });
+
+        }
+
       });
-    }).catch(function (err) {
-      console.log('An error occurred', err);
     })
   );
 
 });
 
 self.addEventListener('notificationclick', function (event) {
-  console.log('notification clicked', event);
   event.notification.close();
 
   event.waitUntil(clients.matchAll({
